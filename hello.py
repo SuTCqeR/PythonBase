c8695f52d0a16c970bca86c48bb71b95fc590c19
SuTcqeR <luiz.otanisc@gmail.com>
2024-08-01 17:18:38 -0300

    Hello world multi linhas, irei colocar uma api com traudor ou fazer direto com uma biblioteca...

--- hello.py
+++ hello.py
@@ -1,2 +1,23 @@
-print("Hello, world!") # Say this, hello world!
-print("Hellows".upper()) #Upper Case
+""" Hello world multi linhas
+
+A depender da sua linguagem definida no sistema, ela mostra o hello word do jeito do seu país
+
+"""
+__version__ = "0.1"
+__author__ = "C0d4c"
+__licence__ = "Unlicense"
+
+
+import os
+
+langs = os.getenv("LANG")[:5]
+
+
+if langs == "en_US":
+    print("Hello, world!")
+elif langs == "pt_BR":
+    print("Olá, mundo!")
+elif langs == "it_IT":
+    print("Bella, ciao!")
+#print("Hello, world!") # Say this, hello world!
+#print("Hellows".upper()) #Upper Case
